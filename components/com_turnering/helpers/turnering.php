16c2c1c44414d28ec32bc7bb73c9da77b1952696
Ray Goat <raygoat@users.noreply.github.com>
2018-01-22 17:56:09 +0100

    Update turnering.php

--- components/com_turnering/helpers/turnering.php
+++ components/com_turnering/helpers/turnering.php
@@ -3,9 +3,9 @@
 /**
  * @version     1.0.0
  * @package     com_turnering
- * @copyright   Copyright (C) 2014 INC Trampa. Alle rettigheder forbeholdes.
+ * @copyright   Copyright (C) 2014 RayGoat. Alle rettigheder forbeholdes.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
- * @author      René Gedde <diverse@oob.dk> - http://oob.dk
+ * @author      Ray Goat
  */
 defined('_JEXEC') or die;
 
